5274b21cfcdab92782f87027e8e91c9714d185a8
phpcpedro <126592396+phpcpedro@users.noreply.github.com>
2025-08-29 09:07:08 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/aula2/Aula2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula2;
+
+/**
+ *
+ * @author aluno
+ */
+public class Aula2 {
+
+    public static void main(String[] args) {
+       
+        pessoa eu = new pessoa();
+        eu.nome = "Henrique";
+        
+        System.out.println(eu.nome);
+        System.out.println("Idade: " + eu.idade);
+        System.out.println("Sexo: " + eu.sexo);
+        System.out.println("Estado Civil: " + eu.estadoCivil);
+        System.out.println("Hobby: " + eu.hobby);
+        
+    }
+}
---
+++ src/main/java/com/mycompany/aula2/pessoa.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aula2;
+
+/**
+ *
+ * @author aluno
+ */
+public class pessoa {
+    
+    String nome = "Carlos";
+    int idade = 25;
+    char sexo = 'M';
+    String estadoCivil = "solteiro";
+    String hobby = "futebol";
+    }
